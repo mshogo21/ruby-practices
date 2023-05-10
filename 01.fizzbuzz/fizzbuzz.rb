879d98aff036a5edc5081a3a2992a5615ab777aa
mshogo21 <mshogo0201@gmail.com>
2023-05-11 05:51:58 +0900

    fizzbuzz課題

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+x = 1
+(1..20).each do |i|
+  if x % 3 == 0 && x % 5 == 0
+    puts "FizzBuzz"
+  elsif x % 3 == 0
+    puts "Fizz"
+  elsif x % 5 == 0
+    puts "Buzz"
+  else
+    puts x
+  end
+  x += 1
+end
